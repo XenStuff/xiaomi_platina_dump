b4f053b0450c9e072099510f8a5c4526444a7194
xenxynon <flynryder427@gmail.com>
2022-06-28 09:20:00 +0000

    Add extras for platina-user 10 QKQ1.190910.002 V12.5.1.0.QDTCNXM release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## platina-user 10 QKQ1.190910.002 V12.5.1.0.QDTCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: sdm660
+- Codename: platina
+- Brand: Xiaomi
+- Flavor: platina-user
+- Release Version: 10
+- Id: QKQ1.190910.002
+- Incremental: V12.5.1.0.QDTCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Xiaomi/platina/platina:10/QKQ1.190910.002/V12.5.1.0.QDTCNXM:user/release-keys
+- OTA version: 
+- Branch: platina-user-10-QKQ1.190910.002-V12.5.1.0.QDTCNXM-release-keys
+- Repo: xiaomi_platina_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/product/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/product/priv-app/Settings/Settings.apk
+rm -f system/system/product/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
